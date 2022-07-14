238ebc0018d8a08e064b6dce9bc52034ee4222a4
4OF <4OF@4of.dev>
2022-07-14 22:02:51 +0900

    Auto install packer

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,3 +1,10 @@
+-- Auto install Packer.nvim
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 vim.cmd[[packadd packer.nvim]]
 
 require'packer'.startup(function()
@@ -24,4 +31,8 @@ require'packer'.startup(function()
     use 'glepnir/dashboard-nvim'
     use 'folke/which-key.nvim'
     use 'folke/tokyonight.nvim'
+    
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
