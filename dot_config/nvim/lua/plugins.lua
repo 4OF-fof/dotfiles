9ddbbbe0ddf2c4a4d11ec2eef8a00f14cedbf10b
4OF <4OF@4of.dev>
2022-08-03 16:48:32 +0900

    WIP

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -20,12 +20,6 @@ require'packer'.startup(function()
         'nvim-telescope/telescope.nvim',
         requires = { 'nvim-lua/plenary.nvim' }
     }
-    use{
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons'
-        }
-    }
     use 'akinsho/toggleterm.nvim'
     use 'windwp/nvim-autopairs'
     use 'glepnir/dashboard-nvim'
